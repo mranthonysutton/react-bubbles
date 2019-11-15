39b3ffabbf8e4724e262f685672386ddedbbdbfa
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-15 08:48:43 -0800

    Built out login page that redirects the user to the bubbles page when they have an active token

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import {Redirect} from "react-router-dom"
+import AxiosWithAuth from "../utils/AxiosWithAuth";
 
 const Login = (props) => {
   // make a post request to retrieve a token from the api
@@ -11,26 +13,47 @@ const Login = (props) => {
     isLoggedIn: false
   });
 
+  // sets isLoggedIn to true if the sessionStorage token is valid
+  useEffect(() => {
+    if (sessionStorage.getItem('token')) {
+      setLoginCredentials({...loginCredentials, isLoggedIn: true});
+    } else {
+      setLoginCredentials({...loginCredentials, isLoggedIn: false});
+    }
+  }, [loginCredentials.isLoggedIn])
+
   // sets login credentials while the user types in their username & password
   const handleChange = event => {
     setLoginCredentials({...loginCredentials, [event.target.name]: event.target.value})
   };
 
+  // Sets the response token to the authorization header, and then redirects the user to the Bubbles page.
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('LOGIN CREDENTIALS: ', loginCredentials);
+    // console.log(loginCredentials);
+    AxiosWithAuth().post('/api/login', loginCredentials).then(response => {
+      const {data} = response;
+      sessionStorage.setItem('token', data.payload);
+      setLoginCredentials({...loginCredentials, isLoggedIn: true});
+      props.history.push('/bubbles');
+    }).catch(error => console.log(error));
   };
 
-  return (
-    <div className="container">
-      <h1>Welcome to the Bubble App!</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name={"username"} placeholder={"Lambda"} onChange={handleChange}/>
-        <input type="password" name={"password"} placeholder={"Lambda"} onChange={handleChange}/>
-        <button type={"submit"}>Log In</button>
-      </form>
-    </div>
-  );
+  // If the login credentials.isLoggedIn is true, then immediately redirects the user to the Bubbles page
+  if (loginCredentials.isLoggedIn === true) {
+    return <Redirect to={"/bubbles"}/>;
+  } else {
+    return (
+      <div className="container">
+        <h1>Login For Bubbles</h1>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name={"username"} placeholder={"Lambda"} onChange={handleChange}/>
+          <input type="password" name={"password"} placeholder={"Lambda"} onChange={handleChange}/>
+          <button type={"submit"}>Log In</button>
+        </form>
+      </div>
+    );
+  }
 };
 
 export default Login;
