10711b87b90c7e95748b664330e1772e1fd692c8
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-15 08:22:18 -0800

    Login form sets the loginCredential state. Need to pass in a test if a token exists

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  // Sets state for the login credentials
+  const [loginCredentials, setLoginCredentials] = useState({
+    username: '',
+    password: '',
+    isLoggedIn: false
+  });
+
+  // sets login credentials while the user types in their username & password
+  const handleChange = event => {
+    setLoginCredentials({...loginCredentials, [event.target.name]: event.target.value})
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('LOGIN CREDENTIALS: ', loginCredentials);
+  };
+
   return (
-    <>
+    <div className="container">
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name={"username"} placeholder={"Lambda"} onChange={handleChange}/>
+        <input type="password" name={"password"} placeholder={"Lambda"} onChange={handleChange}/>
+        <button type={"submit"}>Log In</button>
+      </form>
+    </div>
   );
 };
 
